533d404990bc1b0528797d2c730bf65301fd0e52
Lean Junio <leanjunio@live.com>
2017-09-29 00:37:53 -0400

    added the functionality to view the total market cap

--- client/script.js
+++ client/script.js
@@ -1,3 +1,4 @@
+// initialize callBaseUrl() when the DOM loads
 $(document).ready(function(){
   callBaseUrl();
 });
@@ -9,9 +10,8 @@ var callBaseUrl = function(){
     async: true,
     url: baseUrl,
     method: 'GET',
-    success: function(data){
-      var coins = data;
-      displayView(coins);
+    success: function(data){ // pass the 'data' object into displayView function if success
+      displayView(data);
     },
     error: function(){
       console.log('There was an error in retrieving the data.')
@@ -19,8 +19,12 @@ var callBaseUrl = function(){
   })
 }
 
+// displays the view by rendering and formatting the data into their elements
 var displayView = function(coin){
   
+  var total_el = $('#total');
+  var total = 0;
+
   $.each(coin, function(i) {
 
     // create table row for each coin
@@ -32,19 +36,21 @@ var displayView = function(coin){
     var td_value = $('<td></td>')
     var td_market_cap = $('<td></td>');
     var td_change = $('<td></td>');
-
+    
     th.text(i+1);
     td_name.text(coin[i].name);
     td_symbol.text(coin[i].symbol);
     td_value.text('$ ' + coin[i].price_cad);
     td_market_cap.text(coin[i].market_cap_cad);
     td_change.text(coin[i].percent_change_24h);
-
+    total += Number(coin[i].market_cap_cad);
+    
     tr.append(th, td_name, td_symbol, td_value, td_market_cap, td_change);
     
-    // append the modified 'tr' to the 'tbody'
     $('#data > tbody').append(tr);
-  });
 
+  });
   
+  // get the total market cap value and append it as text for h3#total
+  total_el.text(total);
 }
